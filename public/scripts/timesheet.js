e9b2cdb57affa97444f2bed9666ec2d27d321b99
karolkornecki <karol.kornecki@gmail.com>
2016-09-15 22:05:33 +0200

    initial commit

---
+++ public/scripts/timesheet.js
@@ -0,0 +1,15 @@
+var Footer = React.createClass({
+    render: function() {
+        return (
+            <div class="footer">
+                <p>Karol Kornecki</p>
+            </div>
+        );
+    }
+});
+
+
+ReactDOM.render(
+    <Footer />,
+    document.getElementById('content')
+);
