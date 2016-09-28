8e4633d8b39bdffdd3ea46cbbc5116f8666178d5
Karol.Kornecki <karol.kornecki@gmail.com>
2016-09-28 17:23:27 +0200

    redux forms

--- dev/js/reducers/index.js
+++ dev/js/reducers/index.js
@@ -1,4 +1,6 @@
 import { combineReducers } from 'redux'
+import { reducer as formReducer } from 'redux-form'
+
 import reservationById from '../routes/TimesheetPage/reducers/reservationById'
 import weekdayById from '../routes/TimesheetPage/reducers/weekdayById'
 import availableProjects from '../routes/TimesheetPage/reducers/availableProjects'
@@ -12,7 +14,8 @@ const reducers = combineReducers({
     availableProjects,
     projectById,
     weekDateRangeLabel,
-    registrationUser
+    registrationUser,
+    form: formReducer
 });
 
 export default reducers
--- dev/js/routes/RegistrationPage/components/RegistrationForm.js
+++ dev/js/routes/RegistrationPage/components/RegistrationForm.js
@@ -1,55 +1,124 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+import { connect } from 'react-redux'
 
-import UsernameBox from './UsernameBox'
-import EmailBox from './EmailBox'
-import PasswordBox from './PasswordBox'
-import PasswordConfirmationBox from './PasswordConfirmationBox'
+
+const validate = values => {
+    const errors = {}
+    if (!values.username) {
+        errors.username = 'Your username is required.'
+    } else if (values.username.length < 0) {
+        errors.username = 'Your username is required to be at least 1 characters.'
+    } else if (values.username.length > 50) {
+        errors.username = 'Your username cannot be longer than 50 characters.'
+    } else if (/^[a-z0-9]+$/.test(values.username)) {
+        errors.username = 'Your username can only contain lower-case letters and digits.'
+    }
+
+    if (!values.email) {
+        errors.email = 'Your e-mail is required.'
+    } else if (values.email.length < 5) {
+        errors.email = 'Your e-mail is required to be at least 5 characters..'
+    } else if (values.email.length > 100) {
+        errors.email = 'Your e-mail cannot be longer than 100 characters.'
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+        errors.email = 'Your e-mail is invalid.'
+    }
+
+    if (!values.password) {
+        errors.password = 'Your password is required.'
+    } else if (values.password.length < 4) {
+        errors.password = 'Your password is required to be at least 4 characters.'
+    } else if (values.password.length > 50) {
+        errors.password = 'Your password cannot be longer than 50 characters.'
+    }
+
+    if (!values.confirmation) {
+        errors.confirmation = 'Your confirmation password is required.'
+    } else if (values.confirmation.length < 4) {
+        errors.confirmation = 'Your confirmation password is required to be at least 4 characters.'
+    } else if (values.confirmation.length > 50) {
+        errors.confirmation = 'Your confirmation password cannot be longer than 50 characters.'
+    }
+
+    return errors
+}
+
+
+const renderField = ({ input, label, type, meta: { touched, error } }) => (
+    <div>
+        <input  {...input}
+            placeholder={label}
+            type={type}
+            className="form-control"/>
+
+        <div>
+            <p className="help-block">
+                {touched && meta.invalid && error && <span>{error}</span>}
+            </p>
+        </div>
+    </div>
+)
+
 
 class RegistrationForm extends Component {
 
-    handleSubmit(e) {
-        e.preventDefault();
-
-    }
 
     render() {
         return (
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-8 col-md-offset-2">
-                        <h1>Registration</h1>
+            <form onSubmit={this.props.handleSubmit}>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-8 col-md-offset-2">
+                            <h1>Registration</h1>
 
-                        <div className="alert alert-success">
-                            <strong>Registration saved!</strong> Please check your email for confirmation.
+                            <div className="alert alert-success">
+                                <strong>Registration saved!</strong> Please check your email for confirmation.
+                            </div>
+                            <div className="alert alert-danger">
+                                <strong>Registration failed!</strong> Please try again later.
+                            </div>
+                            <div className="alert alert-danger">
+                                <strong>Login name already registered!</strong> Please choose another one.
+                            </div>
+                            <div className="alert alert-danger">
+                                <strong>E-mail is already in use!</strong> Please choose another one.
+                            </div>
+                            <div className="alert alert-danger">
+                                The password and its confirmation do not match!
+                            </div>
                         </div>
-                        <div className="alert alert-danger">
-                            <strong>Registration failed!</strong> Please try again later.
-                        </div>
-                        <div className="alert alert-danger">
-                            <strong>Login name already registered!</strong> Please choose another one.
-                        </div>
-                        <div className="alert alert-danger">
-                            <strong>E-mail is already in use!</strong> Please choose another one.
-                        </div>
-                        <div className="alert alert-danger">
-                            The password and its confirmation do not match!
-                        </div>
-                    </div>
-                    <div className="col-md-8 col-md-offset-2">
-                        <form name="form" role="form" onSubmit={ (e) => this.handleSubmit(e)}>
-                            <UsernameBox />
-                            <EmailBox />
-                            <PasswordBox />
-                            <PasswordConfirmationBox />
+                        <div className="col-md-8 col-md-offset-2">
+                            <div className="form-group">
+                                <label className="control-label" htmlFor="username">Username</label>
+                                <Field name="username" type="text" component={renderField} label="Username"/>
+                            </div>
+                            <div className="form-group">
+                                <label className="control-label" htmlFor="email">Email</label>
+                                <Field name="email" type="email" component={renderField} label="Email"/>
+                            </div>
+                            <div className="form-group">
+                                <label className="control-label" htmlFor="password">Password</label>
+                                <Field name="password" type="password" component={renderField} label=""/>
+                            </div>
+                            <div className="form-group">
+                                <label className="control-label" htmlFor="confirmation">Password confirmation</label>
+                                <Field name="confirmation" type="password" component={renderField} label=""/>
+                            </div>
                             <button type="submit" className="btn btn-primary">Register</button>
-                        </form>
-                        <p></p>
 
+                            <p></p>
+
+                        </div>
                     </div>
                 </div>
-            </div>
+            </form>
+
         );
     }
 }
-
+RegistrationForm = reduxForm({
+    form: 'registrationForm',
+    validate
+})(RegistrationForm)
 export default RegistrationForm
