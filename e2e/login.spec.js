03f64ca34740ed796bbb47054723c0853b1058fb
karolkornecki <karol.kornecki@gmail.com>
2016-10-08 15:42:23 +0200

    e2e test refactoring

--- e2e/login.spec.js
+++ e2e/login.spec.js
@@ -10,43 +10,37 @@ const By = webdriver.By;
 const until = webdriver.until;
 
 
-describe('login form', ()=> {
+describe('Login page - test suite', ()=> {
 
     beforeEach(done => {
         driver.navigate().to('http://localhost:3000')
-            .then(()=> driver.findElement(By.id('account-menu')))
-            .then((account) => account.click())
-            .then(() => driver.findElement(By.css('a[href="/login"]')))
-            .then((signIn) => signIn.click())
-            .then(()=> done())
+        driver.findElement(By.id('account-menu')).click()
+        driver.findElement(By.css('a[href="/login"]')).click()
+            .then(()=> done()) // without done, tests run 10x slower
     })
 
     it('should login fail if submit with empty username and password', (done)=> {
 
-        driver.findElement(By.id('sign-in'))
-            .then(button => button.click())
-            .then(()=> driver.wait(until.elementLocated(By.id('login-failed')), 3000))
-            .then(() => driver.findElement(By.id('login-failed')))
-            .then((div)=> div.getText())
-            .then(text => expect(text).to.be.equal('Failed to sign in! Please check your credentials and try again.'))
-            .then(()=> driver.getCurrentUrl())
-            .then((url)=> expect(url).to.be.equal('http://localhost:3000/login'))
-            .then(()=>done())
+        driver.findElement(By.id('sign-in')).click()
+        driver.wait(until.elementLocated(By.id('login-failed')))
+        driver.findElement(By.id('login-failed')).getText()
+            .then(text => expect(text).to.equal('Failed to sign in! Please check your credentials and try again.'))
+        driver.getCurrentUrl()
+            .then((url) => expect(url).to.equal('http://localhost:3000/login'))
+            .then(() => done())
 
     })
 
     it('should login fail if incorrect username and password submitted', (done)=> {
 
         driver.findElement(By.css('input[name="username"]')).sendKeys('non_existing_user_name')
-            .then(()=> driver.findElement(By.css('input[name="password"]')).sendKeys('secret'))
-            .then(()=>driver.findElement(By.id('sign-in')))
-            .then(button => button.click())
-            .then(()=> driver.wait(until.elementLocated(By.id('login-failed'))))
-            .then(() => driver.findElement(By.id('login-failed')))
-            .then((div)=> div.getText())
-            .then(text => expect(text).to.be.equal('Failed to sign in! Please check your credentials and try again.'))
-            .then(()=> driver.getCurrentUrl())
-            .then((url)=> expect(url).to.be.equal('http://localhost:3000/login'))
+        driver.findElement(By.css('input[name="password"]')).sendKeys('secret')
+        driver.findElement(By.id('sign-in')).click()
+        driver.wait(until.elementLocated(By.id('login-failed')))
+        driver.findElement(By.id('login-failed')).getText()
+            .then(text => expect(text).to.equal('Failed to sign in! Please check your credentials and try again.'))
+        driver.getCurrentUrl()
+            .then((url)=> expect(url).to.equal('http://localhost:3000/login'))
             .then(()=>done())
 
     })
@@ -54,12 +48,11 @@ describe('login form', ()=> {
     it('should login success if correct login and password submitted', (done)=> {
 
         driver.findElement(By.css('input[name="username"]')).sendKeys('admin')
-            .then(()=> driver.findElement(By.css('input[name="password"]')).sendKeys('admin'))
-            .then(()=> driver.findElement(By.id('sign-in')))
-            .then(button => button.click())
-            .then(()=> driver.wait(until.elementLocated(By.css('a[href="/timesheet"]'))))
-            .then(()=> driver.getCurrentUrl())
-            .then((url)=> expect(url).to.be.equal('http://localhost:3000/'))
+        driver.findElement(By.css('input[name="password"]')).sendKeys('admin')
+        driver.findElement(By.id('sign-in')).click()
+        driver.wait(until.elementLocated(By.css('a[href="/timesheet"]')))
+        driver.getCurrentUrl()
+            .then((url)=> expect(url).to.equal('http://localhost:3000/'))
             .then(()=>done())
 
     })
